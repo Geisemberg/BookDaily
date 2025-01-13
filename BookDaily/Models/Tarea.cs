5dc1f983c94d152d64129e13735a6363e66a3e46
Geisemberg <gzamora@sistecredito.com>
2025-01-12 22:05:08 -0500

    Agregar archivos de proyecto.

---
+++ BookDaily/Models/Tarea.cs
@@ -0,0 +1,13 @@
+﻿namespace BookDaily.Models
+{
+    public class Tarea
+    {
+
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public String Prioridad { get; set; }
+        public bool EstaIncompleta { get; set; }
+
+    }
+}
